d73ec6431cedb93bcf9e46ce95fb2b457d9a79a8
timmy <timmy@van-ons.nl>
2024-01-11 21:32:04 +0100

    refactor

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,15 +3,48 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Services\DatabaseExportService;
+use App\Services\FileDownloadService;
 use App\Services\MessageService;
+use App\Services\PHPScriptRunnerService;
+use App\Services\RunService;
+use App\Services\SessionValidationService;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->app->singleton(MessageService::class, function ($app) {
-            return new MessageService();
+        // PHPScriptRunnerService doesn't seem to have dependencies, so it can be new-ed up directly.
+        $this->app->singleton(PHPScriptRunnerService::class, function ($app) {
+            return new PHPScriptRunnerService();
         });
+
+        // Assuming MessageService depends on PHPScriptRunnerService
+        $this->app->singleton(MessageService::class, function ($app) {
+            return new MessageService($app->make(PHPScriptRunnerService::class));
+        });
+
+        // Assuming RunService depends on PHPScriptRunnerService
+        $this->app->singleton(RunService::class, function ($app) {
+            return new RunService($app->make(PHPScriptRunnerService::class));
+        });
+
+        // Assuming FileDownloadService depends on PHPScriptRunnerService
+        $this->app->singleton(FileDownloadService::class, function ($app) {
+            return new FileDownloadService($app->make(PHPScriptRunnerService::class));
+        });
+
+        // Assuming DatabaseExportService depends on PHPScriptRunnerService
+        $this->app->singleton(DatabaseExportService::class, function ($app) {
+            return new DatabaseExportService($app->make(PHPScriptRunnerService::class));
+        });
+
+        // SessionValidationService doesn't seem to have dependencies, so it can be new-ed up directly.
+        $this->app->singleton(SessionValidationService::class, function () {
+            return new SessionValidationService();
+        });
+
+        // Register other services similarly...
     }
 
     /**
@@ -22,3 +55,4 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 }
+
