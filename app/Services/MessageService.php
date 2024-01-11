004e4034f410693b29a4283d8ef684ba434afb8c
timmy <timmy@van-ons.nl>
2024-01-11 21:22:03 +0100

    refactor

--- app/Services/MessageService.php
+++ app/Services/MessageService.php
@@ -1,53 +1,77 @@
 <?php
-
 namespace App\Services;
 
 use Illuminate\Support\Facades\Session;
-use Symfony\Component\Process\Process;
-use Symfony\Component\Process\Exception\ProcessFailedException;
+use Illuminate\Support\Facades\Cache;
+use App\Services\PHPScriptRunnerService;
 
-class MessageService {
-    private $phpBinaryPath;
-    private $scriptPath;
+class MessageService
+{
+    protected $phpScriptRunnerService;
 
-    public function __construct() {
-        $this->phpBinaryPath = '/Users/dysisx/Library/Application Support/Herd/bin/php';
-        $this->scriptPath = '/Users/dysisx/Documents/assistant/app/Http/Controllers/OpenaiAssistantController.php';
+    public function __construct(PHPScriptRunnerService $phpScriptRunnerService)
+    {
+        $this->phpScriptRunnerService = $phpScriptRunnerService;
     }
 
-    public function submitMessage($request) {
-        $this->validateSession(['assistantId', 'threadId']);
-        $userMessage = $request->input('message');
-        return $this->runPHPScript('addMessage', [Session::get('threadId'), 'user', $userMessage]);
+    public function submitMessage($threadId, $assistantId, $userMessage)
+    {
+        $this->phpScriptRunnerService->runScript('addMessage', [$threadId, 'user', $userMessage]);
     }
 
-    public function getMessages($threadId) {
+    public function getMessages($threadId)
+    {
         return $this->fetchAndProcessMessages($threadId);
     }
 
-    private function fetchAndProcessMessages($threadId) {
-        // Implementation of fetchAndProcessMessages
-    }
+    private function fetchAndProcessMessages($threadId) 
+    {
+        $messagesJson = $this->phpScriptRunnerService->runScript('getMessages', [$threadId]);
+        $messagesData = json_decode($messagesJson, true);
+        $processedMessages = Cache::get('processedMessages', []);
 
-    private function runPHPScript($function, $args = []) {
-        $process = new Process(array_merge([$this->phpBinaryPath, $this->scriptPath, $function], $args));
-        $process->setWorkingDirectory(base_path());  
-        $process->run();
-
-        if (!$process->isSuccessful()) {
-            throw new ProcessFailedException($process);
-        }
-
-        return trim($process->getOutput());
-    }
-
-    private function validateSession(array $keys) {
-        foreach ($keys as $key) {
-            if (!Session::has($key)) {
-                throw new \Exception("$key not found in session.");
+        foreach ($messagesData as $key => $message) {
+            if (isset($processedMessages[$message['id']])) {
+                continue; 
             }
+
+            $messagesData[$key]['fileId'] = $this->processMessageForFileId($threadId, $message);
+            $processedMessages[$message['id']] = 1;
         }
+
+        Cache::put('processedMessages', $processedMessages, 3600);
+        return $messagesData;
     }
 
-    // Other methods related to messages...
+    private function processMessageForFileId($threadId, $message)
+    {
+        $fileIdsJson = $this->phpScriptRunnerService->runScript('listMessageFiles', [$threadId, $message['id']]);
+        $fileIds = json_decode($fileIdsJson, true);
+
+        if (!empty($fileIds) && is_array($fileIds)) {
+            $fileId = $fileIds[0];
+            $this->storeFileMetadataInSession($fileId, $threadId, $message['id'], $message['content']);
+            return $fileId;
+        }
+
+        return null;
+    }
+
+    private function storeFileMetadataInSession($fileId, $threadId, $messageId, $content)
+    {
+        $fileName = $this->extractFileNameFromContent($content);
+        Session::put($fileId . '-threadId', $threadId);
+        Session::put($fileId . '-messageId', $messageId);
+        Session::put($fileId . '-fileName', $fileName);
+    }
+
+    private function extractFileNameFromContent($content)
+    {
+        if (preg_match('/\[Download (.*?)\]\(sandbox:/', $content, $matches)) {
+            return $matches[1];
+        }
+
+        return 'defaultFileName.txt';
+    }
 }
+
