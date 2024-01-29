077929c5a5c0d1483b6f6780595c9a7cf1c0064d
timmy <timmy@van-ons.nl>
2024-01-29 19:19:28 +0100

    routes auth

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OpenAIController;
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Auth;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-*/
 
-// Default Breeze route for welcome page
 
-// Original OpenAIController routes
-Route::get('/', [OpenAIController::class, 'index']);
 
-// Routes that require authentication
+
 Route::middleware('auth')->group(function () {
+    Route::get('/', [OpenAIController::class, 'index']);
     Route::post('/submit-message', [OpenAIController::class, 'submitMessage']);
     Route::post('/delete-thread/{threadId}', [OpenAIController::class, 'deleteThread']);
     Route::post('/cancel-run', [OpenAIController::class, 'cancelRun']);
@@ -30,16 +24,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/get-threads', [OpenAIController::class, 'getThreads']);
     Route::post('/thread/create-run', [OpenAIController::class, 'createAndRunThreadWithMessage']); // New route
 
-    // Profile routes
+ 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-// Default Breeze routes for dashboard
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-// Include the Breeze auth routes
 require __DIR__.'/auth.php';
